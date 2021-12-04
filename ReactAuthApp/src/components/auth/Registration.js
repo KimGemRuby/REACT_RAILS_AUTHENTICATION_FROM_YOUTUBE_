4dcc1ca6c62b150b6322c9c91a221c0c2a5ec975
albator1983 <kim13kara@gmail.com>
2021-12-04 20:28:02 +0100

    debut_video6_building_react_registration_componnent

--- ReactAuthApp/src/components/auth/Registration.js
+++ ReactAuthApp/src/components/auth/Registration.js
@@ -1,1 +1,13 @@
  import React, { Component} from "react"
+
+ export default class Registration extends Component {
+   render() {
+     return (
+       <div>
+           <h1>Home</h1>
+           <h1>Home</h1>
+         <h1>Registration</h1>
+       </div>
+     )
+   }
+ }
